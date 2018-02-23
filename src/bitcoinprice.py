1fd9547e8547e705684b2cd8e8de7982894f8bb3
Thanapon Sathirathiwat <tsathira@uci.edu>
2018-02-22 16:55:48 -0800

    better error handler if url given not found

--- src/bitcoinprice.py
+++ src/bitcoinprice.py
@@ -7,12 +7,17 @@ if (len(sys.argv)== 2 and len(sys.argv[1]) == 3):
     currency = str(sys.argv[1]).upper()
 
     # get current price from coindesk api
-    r = requests.get(url='https://api.coindesk.com/v1/bpi/currentprice/' + currency + '.json')
+    try:
+        r = requests.get(url='https://api.coindesk.com/v1/bpi/currentprice/' + currency + '.json')
+        r.raise_for_status()
+        # translate the json file and grab information on bitcoin price only
+        currencyJson = r.json()['bpi']
 
-    # translate the json file and grab information on bitcoin price only
-    currencyJson = r.json()['bpi']
-
-    # access the bitcoin price in string format with key "rate"
-    print(currencyJson[currency]['rate']+ ' '+ currency  ) 
+        # access the bitcoin price in string format with key "rate"
+        print(currencyJson[currency]['rate']+ ' '+ currency  ) 
+    except requests.exceptions.HTTPError as err:
+        print(err)
+        sys.exit(1)
+    
 else:
     print("Invalid argument given.")
